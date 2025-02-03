d8ea1b56fce194b5eeca79bd76778549a08631cf
devasaya2003 <devasyasing2000@gmail.com>
2025-02-03 21:02:31 +0530

    Middleware configured successfully

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,36 @@
+import { withAuth } from "next-auth/middleware";
 import { NextRequest, NextResponse } from "next/server";
-import { jwtVerify } from "jose";
+
+export default withAuth(
+  function middleware(req: NextRequest) {
+    if (
+      req.method === "GET" &&
+      req.nextUrl.pathname.startsWith("/api/v1/resources")
+    ) {
+      return NextResponse.next();
+    }
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        if (
+          req.method === "GET" &&
+          req.nextUrl.pathname.startsWith("/api/v1/resources")
+        ) {
+          return true;
+        }
+        return !!token;
+      },
+    },
+  }
+);
 
 export const config = {
   matcher: [
+    "/api/v1/resources/:path*",
     "/api/v1/companies/:path*",
     "/api/v1/degrees/:path*",
     "/api/v1/skills/:path*",
   ],
 };
-
-export async function middleware(req: NextRequest) {
-  if (
-    req.method === "GET" &&
-    req.nextUrl.pathname.startsWith("/api/v1/resources")
-  ) {
-    return NextResponse.next();
-  }
-  const authHeader = req.headers.get("Authorization");
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-
-  const token = authHeader.split(" ")[1];
-
-  try {
-    const secret = new TextEncoder().encode(process.env.NEXTAUTH_SECRET);
-
-    const { payload } = await jwtVerify(token, secret);
-
-    return NextResponse.next();
-  } catch (error) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-}
