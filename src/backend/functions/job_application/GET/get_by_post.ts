344363e7a22e7413da4d69031283a0ef873eae05
devasaya2003 <devasyasing2000@gmail.com>
2025-02-22 01:48:41 +0530

    Added pagination in the get by post methods

--- src/backend/functions/job_application/GET/get_by_post.ts
+++ src/backend/functions/job_application/GET/get_by_post.ts
@@ -5,21 +5,22 @@ import { getByCompanyAndRecruiterID } from "./GET_BY_POST_FUNCTIONS/by_company_r
 
 interface GetJobsByPost {
   type: string;
-  id: string; // company-id or recruiter-id
-  extra_id: Partial<string>; // only when company-id and recruiter-id both required. This will always be recruiter-id
+  id: string;
+  page_no: number;
+  extra_id: Partial<string>;
 }
 
 export const getJobsByPost = async (data: GetJobsByPost) => {
   const identifier = data.type;
   switch (identifier) {
     case BOTH:
-      getByCompanyAndRecruiterID(data.id, data.extra_id);
+      getByCompanyAndRecruiterID(data.id, data.extra_id, data.page_no);
       break;
     case COMPANY:
-      getByCompanyID(data.id);
+      getByCompanyID(data.id, data.page_no);
       break;
     case RECRUITER:
-      getByRecruiterID(data.id);
+      getByRecruiterID(data.id, data.page_no);
       break;
     default:
       return "Wrong identifier!";
