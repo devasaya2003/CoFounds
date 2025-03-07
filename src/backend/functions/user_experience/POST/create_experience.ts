19bdd536f46b699321d56fb7bb4a3b2e9d534108
devasaya2003 <devasyasing2000@gmail.com>
2025-03-07 21:00:04 +0530

    User experience post api created

---
+++ src/backend/functions/user_experience/POST/create_experience.ts
@@ -0,0 +1,25 @@
+import prisma from "../../../../../prisma/client";
+
+export interface CreateUserExperience {
+  user_id: string;
+  end_at: Date;
+  company_name: string;
+  title: string;
+  description: string;
+  is_active: boolean;
+}
+
+export const createUserExperience = async (data: CreateUserExperience) => {
+  return prisma.userExperience.create({
+    data: {
+      userId: data.user_id,
+      endAt: data.end_at,
+      companyName: data.company_name,
+      title: data.title,
+      description: data.description ?? null,
+      isActive: data.is_active ?? true,
+      createdBy: data.user_id,
+      updatedBy: data.user_id
+    },
+  });
+};
