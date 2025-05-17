a8b41e8ec66a7b50385208e0f06663e77eff5d70
Devasaya2003 <devasyasingh2000@gmail.com>
2025-05-18 04:09:19 +0530

    Added missing analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from '@/redux/provider';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,8 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
         <Providers>
-          {children}
+          {children}        
         </Providers>
+        <Analytics/>
       </body>
     </html>
   );
